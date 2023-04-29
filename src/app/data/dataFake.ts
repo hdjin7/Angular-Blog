c6cc735951ba93766f39c5f85ee87819a36b9e25
hdjin <hebertfbarbosa@hotmail.com>
2023-04-29 02:33:48 -0300

    Projeto de Blog Angular finalizado
    Adicionado dinamismo na passagem de dados para as propriedades dos componentes (no home.component.html), utilizando o ngFor, como proposto nas aulas finais do projeto.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,15 +10,21 @@ export const dataFake = [
     "photo": "https://cinepop.com.br/wp-content/uploads/2021/07/motoqueiro-fantasma-norman-reedus-keanu-reeves-1.jpg",
     "title": "Novo filme do Motoqueiro fantasma a caminho?",
     "description": "Rumores de que a Marvel está trabalho em um novo filme do Motoqueiro fantasma."
-  }, ,
+  },
   {
     "id": "3",
+    "photo": "https://rollingstone.uol.com.br/media/uploads/cavaleiro-da-lua-oscar-isaac-marvel-foto-reproducao-disney.jpg",
+    "title": "Marvel anuncia 2ª temporada de Cavaleiro da Lua",
+    "description": "Marvel acaba de oficializar 2ª temporada de Cavaleiro da Lua para o Disney +. Não foi informada uma data de estreia, mas sua previsão é para o segundo semestre de 2024."
+  },
+  {
+    "id": "4",
     "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
     "title": "Saiba mais sobre a series da Marvel",
     "description": "Novas series da anunciadas pela Marvel ganha data de estreia no Disney +."
-  }, ,
+  },
   {
-    "id": "4",
+    "id": "5",
     "photo": "https://revistaanamaria.com.br/media/_versions/2022-05/no-way-home-hbo-max_widemd.jpg",
     "title": "Homem Aranha: Sem Volta Para Casa ganha data de estreia",
     "description": "Divulgada data de estreia de Homem Aranha: Sem Volta Para Casa, que marca a volta de Andrew e Tobey com suas versões de Homem Aranha e da inicio ao aranha-verso no UCM."
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  result: {
+    idContent: string | undefined, photoCover: string | undefined,
+    titleContent: string | undefined,
+    descriptionContent: string | undefined
+  }[] = [];
+
+  ngOnInit(): void {
+    this.setValuesToComponent()
+  }
+
+  setValuesToComponent() {
+    // Adicionando dados do dataFake no array
+    dataFake.forEach((item) => {
+      this.result.push({
+        idContent: item?.id,
+        photoCover: item?.photo,
+        titleContent: item?.title,
+        descriptionContent: item?.description,
+      });
+    });
+  }
 }
